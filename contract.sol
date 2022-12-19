62c0f0d4a840a4633e3527b6172d4f4b077a987f
montsecruz9 <104812189+montsecruz9@users.noreply.github.com>
2022-12-19 12:26:23 -0600

    creating and declaring event
    created event for the app front-end

--- contract.sol
+++ contract.sol
@@ -6,6 +6,9 @@ contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
+    // Creating event for app front-end
+    event NewZombie(uint zombieId, string name, uint dna);
+
     // Create zombies
     struct Zombie {
         string name;
@@ -16,7 +19,21 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function createZombie (string memory _name, uint _dna) public {
-        // add zombies to the array
-        zombies.push(Zombie(_name, _dna));
+        // adding zombies to array and declaring zombie's ids 
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // declaring event
+        emit NewZombie(id, _name, _dna);
+    }
+
+    // generating random dna for zombies
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str));
+        return rand % dnaModulus; //our dna will only be 16 digits long
+    }
+
+    // randomly creating zombies 
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 }
