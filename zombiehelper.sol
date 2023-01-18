4f0f981937b2769e8a88d2eeb83057e09748c913
montsecruz9 <104812189+montsecruz9@users.noreply.github.com>
2023-01-18 16:06:51 -0600

    declaring array

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -23,4 +23,10 @@ contract ZombieHelper is ZombieFeeding {
 
   }
 
+  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    
+    return result;
+  }
+
 }
