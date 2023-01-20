717160fbcf3559a7d7ee8739797cda93579d75de
montsecruz9 <104812189+montsecruz9@users.noreply.github.com>
2023-01-20 15:26:20 -0600

    updating code
    added ownerOf modifier and storage pointer

--- zombieattack.sol
+++ zombieattack.sol
@@ -11,7 +11,12 @@ contract ZombieAttack is ZombieHelper {
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
     } 
     
-    function attack(uint _zombieId, uint _targetId) external {
+    function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+        // get a storage pointer to both zombies so we can easily interact with them
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        // get a random number between 0 and 99 to determine the battle outcome
+        uint rand = randMod(100);
         
     }
 }
